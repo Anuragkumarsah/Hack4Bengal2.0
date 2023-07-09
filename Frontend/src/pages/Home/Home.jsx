7c68fbc10c7de1d594d48d2831159778771c9718
Anuragkumarsah <asah2307@gmail.com>
2023-07-09 14:06:13 +0530

    added homepage and FAQSection

---
+++ Frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,71 @@
+import "./Home.css";
+
+import { Link } from "react-router-dom";
+
+
+import FAQSection from "../components/FAQSection";
+
+
+import faqData from "../assets/json-data/faqData.json";
+
+
+import services_menu from "../assets/json-data/services_menu.json";
+
+
+
+const Home = () => {
+  return (
+    <>
+      <div className="home">
+        {/* Top Body */}
+        <div className="topBody_container">
+          {/* hero section */}
+          <div className="topBody">
+            <img src="" alt="" />
+            <div className="hero-text">
+              <h1>Empowering Healthcare through Artificial Intelligence</h1>
+              <p>
+                Detect. Connect. Heal. Seamlessly schedule doctor meetings after
+                disease detection, unlocking your path to optimal health.
+              </p>
+              <Link type="button" to="/appointment" className="appointment-btn">
+                Make Appointment
+              </Link>
+            </div>
+          </div>
+          <img className="wave-img" src="" alt="" />
+        </div>
+        {/* Models Section */}
+        <div className="modelSection">
+          {/* Models */}
+          <p>
+            <span>Health At Your FingerTips.</span> <br /> Check your health
+            with our latest AI Technology
+          </p>
+          <div className="models">
+            {services_menu.items.map((item, index) => {
+              if (index !== services_menu.items.length - 1) {
+                return (
+                  <button key={index} className="CtScan">
+                  <img src={item.src} />
+                  <p>{item.title}</p>
+                </button>
+                );
+              }
+            })}
+          </div>
+        </div>
+        <hr />
+        <div className="bottomBody">
+          {/* About */}
+          <div className="faq">
+            <h1>Have a question ?</h1>
+            <FAQSection faqData={faqData} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
