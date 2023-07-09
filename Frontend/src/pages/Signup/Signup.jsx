e6bb8fd377ef2e42463b04f1f1d38c1d9c7b11f0
Indrajit Pal <palindrajit10@gmail.com>
2023-07-09 23:27:43 +0530

    signup page completed

--- Frontend/src/pages/Signup/Signup.jsx
+++ Frontend/src/pages/Signup/Signup.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../Signup/Signup.css'
 import SignupImg from '../../assets/Images/Signup_page_image.png'
 import { Link, useNavigate } from "react-router-dom";
+import bcrypt from "bcryptjs";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import axios from 'axios'
 import { userSignupRoute } from '../../Utils/APIRoutes';
@@ -75,6 +79,101 @@ const Signup = () => {
         confirmPassword,
     } = state;
 
+    const validateForm = () => {
+        if (phoneNumber.length < 10) {
+            toast.error("Phone Number must be at least 10 characters long.");
+            return false;
+        }
+        if (username.length < 3) {
+            toast.error("Username must be at least 3 characters long.");
+            return false;
+        }
+        if (email.trim() === "") {
+            toast.error("Please enter an email.");
+            return false;
+        }
+        if (password.length < 8) {
+            toast.error('Password must be at least 8 characters long.');
+            return false;
+        }
+        if (!/[A-Z]/.test(password)) {
+            toast.error('Password must contain at least one uppercase letter.');
+            return false;
+        }
+        if (!/[a-z]/.test(password)) {
+            toast.error('Password must contain at least one lowercase letter.');
+            return false;
+        }
+        if (!/\d/.test(password)) {
+            toast.error('Password must contain at least one digit.');
+            return false;
+        }
+        if (!/[@#$%^&*]/.test(password)) {
+            toast.error('Password must contain at least one special character (@, #, $, %, ^, &, *).');
+            return false;
+        }
+        if (password !== confirmPassword) {
+            toast.error("Passwords do not match.");
+            return false;
+        }
+        return true;
+    };
+
+    const handleSubmitForm = async () => {
+        if (!validateForm()) {
+            return;
+        }
+
+        try {
+            const hashedPassword = await bcrypt.hash(password, 10);
+            await axios
+                .post(userSignupRoute, {
+                    username,
+                    email,
+                    phoneNumber,
+                    gender,
+                    dob,
+                    password: hashedPassword,
+                })
+                .then((res) => {
+                    console.log(res);
+                    console.log(res.data);
+                    // Additional actions or redirection can be performed here
+                    toast.success("User created successfully!");
+                    setTimeout(() => {
+                        navigate("/login");
+                    }, 2000);
+                })
+                .catch((error) => {
+                    // Error message for existing email
+                    console.error("Error while sending signup request:", error);
+                    if (
+                        error.response &&
+                        error.response.data &&
+                        error.response.data.error === "Email already exists"
+                    ) {
+                        toast.error("Email already exists. Please use a different email.");
+                    }
+                    // Error message for existing username
+                    else if (
+                        error.response &&
+                        error.response.data &&
+                        error.response.data.error === "Username already exists"
+                    ) {
+                        toast.error(
+                            "Username already exists. Please use a different username."
+                        );
+                    } else {
+                        toast.error("An error occurred while signing up.");
+                    }
+                });
+        } catch (error) {
+            console.error("Error while sending signup request:", error);
+            toast.error("An error occurred while signing up.");
+        }
+    };
+
+
     return (
         <div className="signup-page">
             <div className="left-section">
@@ -92,21 +191,21 @@ const Signup = () => {
                             Username
                             <input
                                 type="text"
-                                // value={username}
+                                value={username}
                                 name="username"
                                 className="form-input input"
                                 required // Make username required
-                            // onChange={handleChange}
+                                onChange={handleChange}
                             />
                         </label>
                         <label className="label">
                             Phone Number
                             <input
                                 type="text"
-                                // value={phoneNumber}
+                                value={phoneNumber}
                                 name="phoneNumber"
                                 className="form-input input"
-                            // onChange={handleChange}
+                                onChange={handleChange}
                             />
                         </label>
                     </div>
@@ -115,10 +214,10 @@ const Signup = () => {
                         <label className="label">
                             Gender
                             <select
-                                // value={gender}
+                                value={gender}
                                 name="gender"
                                 className="form-input input"
-                            // onChange={handleChange}
+                                onChange={handleChange}
                             >
                                 <option value="">Select</option>
                                 <option value="M">Male</option>
@@ -129,22 +228,22 @@ const Signup = () => {
                             Age
                             <input
                                 type="date"
-                                // value={dob}
+                                value={dob}
                                 name="dob"
                                 className="form-input input"
-                            // onChange={handleChange}
+                                onChange={handleChange}
                             />
                         </label>
                     </div>
                     <label className="label">
                         Email
                         <input
-                            // value={email}
+                            value={email}
                             type="email"
                             name="email"
                             className="form-input input"
                             required // Make email required
-                        // onChange={handleChange}
+                            onChange={handleChange}
                         />
                     </label>
                     <div className="name-number">
@@ -152,29 +251,34 @@ const Signup = () => {
                             Password
                             <input
                                 type="password"
-                                // value={password}
+                                value={password}
                                 name="password"
                                 className="form-input input"
                                 required // Make password required
-                            // onChange={handleChange}
+                                onChange={handleChange}
                             />
                         </label>
                         <label className="label">
                             Confirm Password
                             <input
                                 type="password"
-                                // value={confirmPassword}
+                                value={confirmPassword}
                                 name="confirmPassword"
                                 className="form-input input"
-                            // onChange={handleChange}
+                                onChange={handleChange}
                             />
                         </label>
                     </div>
 
-                    <span className="password-requirements">
-                        <ul className="password-errors">
-                        </ul>
-                    </span>
+                    {passwordErrors.length > 0 && (
+                        <span className="password-requirements">
+                            <ul className="password-errors">
+                                {passwordErrors.map((error, index) => (
+                                    <li key={index}>{error}</li>
+                                ))}
+                            </ul>
+                        </span>
+                    )}
 
 
                     {/* <span className="password-requirements">
@@ -184,7 +288,7 @@ const Signup = () => {
                         <button
                             type="button"
                             className="action_btn"
-                        // onClick={handleSubmitForm}
+                            onClick={handleSubmitForm}
                         >
                             Sign Up
                         </button>
@@ -198,4 +302,9 @@ const Signup = () => {
     );
 };
 
-export default Signup
+export default () => (
+    <>
+        <ToastContainer position="bottom-right" theme="colored" />
+        <Signup />
+    </>
+);
