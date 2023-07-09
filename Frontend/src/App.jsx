f7b366c6b0ecf1c8647086a42a693cbe2825c400
krishnendu roy <krishnendu.kalyani1@gmail.com>
2023-07-10 00:08:34 +0530

    added braintumer page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,3 @@
-
 import { useState } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
@@ -6,16 +5,18 @@ import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import Cancer from "./pages/Prediction/Cancer";
 import Chat from "./components/Chat/Chat";
-import Login from './pages/Login/Login'
+import Braintumer from "./pages/Prediction/Braintumer";
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route path='/login' element={<Login/>} />
+        <Route exact path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/prediction/cancer" element={<Cancer />} />
+        <Route path="/prediction/brain-tumor" element={<Braintumer />} />
         <Route path="/general/chat" element={<Chat />} />
       </Routes>
     </Router>
